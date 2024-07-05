608f328df874e6aefe463ef3fbb2d1fb294bf58b
Skanda Prasad <skandaprasad595@gmail.com>
2024-07-05 20:22:11 +0530

    Problem 2

---
+++ Day_1/Assignment/Greetings.java
@@ -0,0 +1,15 @@
+//  Take name as input and print a greeting message for that particular name.
+
+package Day_1.Assignment;
+
+import java.util.Scanner;
+
+public class Greetings {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Please enter your name :- ");
+        String name = input.next();
+        System.out.println("Hello " + name);
+        input.close();
+    }
+}
