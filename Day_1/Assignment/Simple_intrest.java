785c8c8c1e3b7df9b3b279849d2eb9451176f1d0
Skanda Prasad <skandaprasad595@gmail.com>
2024-07-05 20:30:26 +0530

    problem 3

---
+++ Day_1/Assignment/Simple_intrest.java
@@ -0,0 +1,21 @@
+//  Write a program to input principal, time, and rate (P, T, R) from the user and find Simple Interest.
+
+package Day_1.Assignment;
+
+import java.util.Scanner;
+
+public class Simple_intrest {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("---Simple intrest--");
+        System.out.print("Principle :- ");
+        int princ = input.nextInt();
+        System.out.print("Time(years):- ");
+        int time = input.nextInt();
+        System.out.print("Rate(%) :- ");
+        int rate = input.nextInt();
+        int intrest = (princ * time * rate) / 100;
+        System.out.println("The intrest is :- " + intrest);
+        input.close();
+    }
+}
