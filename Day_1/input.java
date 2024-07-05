1e4ea067a97b66ed36de02c408405787e1f88af3
Skanda Prasad <skandaprasad595@gmail.com>
2024-07-05 18:21:32 +0530

    inputs

--- Day_1/input.java
+++ Day_1/input.java
@@ -3,13 +3,17 @@ package Day_1;
 import java.util.Scanner;
 
 public class input {
-    public static void main(String[] args) {
+    pub lic
+
+    static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.println(input.nextInt());
-        // Takes the input of integers..
+        // // Takes the input of integers..
         System.out.println(input.nextLine());
-        // prints complete string from the input...
+        // // prints complete string from the input...
         System.out.println(input.next());
-        // prints only first string from the input.....
+        // // prints only first string from the input.....
+        int rollno = input.nextInt();
+        System.out.println("Your roll no is " + rollno);
     }
 }
