bb82539b86be115d31edd5258c49fc214145d27f
Skanda Prasad <skandaprasad595@gmail.com>
2024-07-05 20:17:44 +0530

    Assignments

---
+++ Day_1/Assignment/evenOrodd.java
@@ -0,0 +1,17 @@
+package Day_1.Assignment;
+
+import java.util.Scanner;
+
+public class evenOrodd {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Give a number to check whether it's a even or odd :-");
+        int num = input.nextInt();
+        if (num % 2 == 0) {
+            System.out.println("It's a even number");
+        } else {
+            System.out.println("It's a odd number..");
+        }
+        input.close();
+    }
+}
--- Day_1/primitives.java
+++ Day_1/primitives.java
@@ -4,13 +4,13 @@ public class primitives {
     public static void main(String[] args) {
         String name = "Max";
         // string is not a primitive...
-        int rollno = 85;
-        char grade = 'A';
-        float marks = 92.55f;
-        double largedecno = 54.2525966;
-        long largeint = 5659894846468468L;
-        boolean check = true;
+        // int rollno = 85;
+        // char grade = 'A';
+        // float marks = 92.55f;
+        // double largedecno = 54.2525966;
+        // long largeint = 5659894846468468L;
+        // boolean check = true;
 
-        System.out.println(rollno);
+        System.out.println(name);
     }
 }
