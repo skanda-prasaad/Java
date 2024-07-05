bc6eafa39c164b4ce6aa6681aa835f06d884ffd5
Skanda Prasad <skandaprasad595@gmail.com>
2024-07-05 18:07:44 +0530

    primitives

---
+++ Day_1/primitives.java
@@ -0,0 +1,16 @@
+package Day_1;
+
+public class primitives {
+    public static void main(String[] args) {
+        String name = "Max";
+        // string is not a primitive...
+        int rollno = 85;
+        char grade = 'A';
+        float marks = 92.55f;
+        double largedecno = 54.2525966;
+        long largeint = 5659894846468468L;
+        boolean check = true;
+
+        System.out.println(rollno);
+    }
+}
