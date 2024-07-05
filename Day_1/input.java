bc9509144cbeadb95b1d9f323f97c9bbaafe0bbb
Skanda Prasad <skandaprasad595@gmail.com>
2024-07-05 17:59:17 +0530

    inputs

---
+++ Day_1/input.java
@@ -0,0 +1,15 @@
+package Day_1;
+
+import java.util.Scanner;
+
+public class input {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println(input.nextInt());
+        // Takes the input of integers..
+        System.out.println(input.nextLine());
+        // prints complete string from the input...
+        System.out.println(input.next());
+        // prints only first string from the input.....
+    }
+}
