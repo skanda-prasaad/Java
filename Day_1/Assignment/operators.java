a314d2d3bef515dc1e8c51281a157dd312dbfca0
Skanda Prasad <skandaprasad595@gmail.com>
2024-07-05 21:07:43 +0530

    calculator

---
+++ Day_1/Assignment/operators.java
@@ -0,0 +1,37 @@
+// Take in two numbers and an operator (+, -, *, /) and calculate the value. (Use if conditions)
+
+package Day_1.Assignment;
+
+import java.util.Scanner;
+
+public class operators {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("---Simple Calculator--");
+        System.out.print("Enter first number :- ");
+        int num1 = input.nextInt();
+        System.out.print("Enter second number :- ");
+        int num2 = input.nextInt();
+        System.out.print("Enter the operator (+,-,/,*) :- ");
+        char operator = input.next().charAt(0);
+        double result;
+        if (operator == '+') {
+            result = num1 + num2;
+            System.out.println("Addition value is :- " + result);
+        } else if (operator == '-') {
+            result = num1 - num2;
+            System.out.println("Subtraction value is :- " + result);
+        } else if (operator == '*') {
+            result = num1 * num2;
+            System.out.println("Multiplication value is :- " + result);
+        } else if (operator == '/') {
+            if (num2 == 0) {
+                System.out.println("Division by zero error");
+            } else {
+                result = num1 / num2;
+                System.out.println("Division value is :- " + result);
+            }
+        }
+        input.close();
+    }
+}
