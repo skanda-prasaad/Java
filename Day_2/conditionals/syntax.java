93cd1c75b2991329d2a18fd1c296864dd58ffa5b
Skanda Prasad <skandaprasad595@gmail.com>
2024-07-06 18:29:22 +0530

    conditionals

---
+++ Day_2/conditionals/syntax.java
@@ -0,0 +1,23 @@
+package Day_2.conditionals;
+
+public class syntax {
+    public static void main(String[] args) {
+        /*
+         * Syntax of if statements :-
+         * if(boolean expression T or F){
+         * body }
+         * else {
+         * do this
+         * }
+         */
+        //// Salary example //
+        int salary = 25000;
+
+        if (salary > 15000) {
+            salary += 2000;
+        } else {
+            salary += 1000;
+        }
+        System.out.println(salary);
+    }
+}
