2dd46ed657b4fbdf9b9ff5db1790298880c6abbe
Skanda Prasad <skandaprasad595@gmail.com>
2024-07-06 18:46:07 +0530

    do_while

---
+++ Day_2/loops/do_while.java
@@ -0,0 +1,13 @@
+package Day_2.loops;
+
+public class do_while {
+    public static void main(String[] args) {
+
+        // Q :- Print numbers from 0 to 5
+        int i = 1;
+        do {
+            System.out.println(i);
+            i++;
+        } while (i != 6);
+    }
+}
