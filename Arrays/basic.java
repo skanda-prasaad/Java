9fd4b27fae66583097e9579cc057edb03b5411b8
Skanda Prasad <skandaprasad595@gmail.com>
2024-08-09 12:31:49 +0530

    Started_arr

---
+++ Arrays/basic.java
@@ -0,0 +1,25 @@
+package Arrays;
+
+public class basic {
+    public static void main(String[] args) {
+        // Store a roll number:-
+        int a = 52;
+        // Store a preson's name:-
+        String name = "Skanda";
+
+        // Store 5 roll numbers:-
+        //For this we use arrays :-
+          // Syntax ---->
+           //datatype[] variable_name = new datatype[size];
+
+        // Store 5 roll num :-
+        int[] r_no = new int[5];
+        // or directly
+        int[] r_nums = {25, 52, 95, 45}; 
+
+        // System.out.println(r_no[1]);
+
+        String[] arr = new String[4];
+        System.out.println(arr[0]);
+    }
+}
