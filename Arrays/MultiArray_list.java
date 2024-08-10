cac8b429d570ecefa7b2e3ee351da74bf2b450ef
Skanda Prasad <skandaprasad595@gmail.com>
2024-08-10 18:58:08 +0530

    Array_comp

---
+++ Arrays/MultiArray_list.java
@@ -0,0 +1,20 @@
+package Arrays;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class MultiArray_list {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        ArrayList<ArrayList<Integer>> list = new ArrayList<>();
+        for (int i = 0; i < 3; i++) {
+            list.add(new ArrayList<>());
+        }
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                list.get(i).add(input.nextInt());
+            }
+        }
+        System.out.println(list);
+        input.close();
+    }
+}
