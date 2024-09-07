26fa5a7b78aad7ccc32db117312018b6ce74a9c6
Skanda Prasad <skandaprasad595@gmail.com>
2024-09-07 22:26:13 +0530

    comit

--- Arrays/Array_list_eg.java
+++ Arrays/Array_list_eg.java
@@ -9,21 +9,21 @@ public class Array_list_eg {
         // arraylist are used if there is no value specified for row.
         // Syntax :-
         ArrayList<Integer> list = new ArrayList<>(10);
-        // list.add(20);
-        // list.add(25);
-        // list.add(63);
-        // list.set(1, 85); // set is used to change the value
-        // list.remove(2);
-        // System.out.println(list);
+        list.add(20);
+        list.add(25);
+        list.add(63);
+        list.set(1, 85); // set is used to change the value
+        list.remove(2);
+        System.out.println(list);
 
         // input :-
-        for (int i = 0; i < 5; i++) {
-            list.add(input.nextInt());
-        }
-        // get item at any index :-
-        for (int i = 0; i < 5; i++) {
-            System.out.print(list.get(i)+ " "); // pass index here, list[index] will not work here.
-        }
+        // for (int i = 0; i < 5; i++) {
+        //     list.add(input.nextInt());
+        // }
+        // // get item at any index :-
+        // for (int i = 0; i < 5; i++) {
+        //     System.out.print(list.get(i)+ " "); // pass index here, list[index] will not work here.
+        // }
 
         input.close();
     }
