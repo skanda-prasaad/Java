a49ee73c4942ef5cd2d49d6aed839d21fd0f1063
Skanda Prasad <skandaprasad595@gmail.com>
2025-01-01 15:20:12 +0530

    till pat 6

---
+++ pattern/pat3.java
@@ -0,0 +1,14 @@
+package pattern;
+
+public class pat3 {
+    public static void main(String[] args) {
+        for(int i = 1; i <= 5; i++)
+        {
+            for(int j = 0; j < i; j++)
+            {
+                System.out.print(i);
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ pattern/pat6.java
@@ -0,0 +1,18 @@
+package pattern;
+
+public class pat6 {
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i++){
+            for(int j = 0; j < i; j++){
+                System.out.print(" ");
+            }
+            for(int j = 0; j < 2*(5-i)-1; j++){
+                System.out.print("*");
+            }
+            for(int j = 0; j < i; j++) {
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+}
